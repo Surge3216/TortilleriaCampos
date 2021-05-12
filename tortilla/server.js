d80b21bb3a8d7426d3647e31f4667e1507126b39
Aubrey Mark <aubsmark@gmail.com>
2021-05-11 21:55:09 -0600

    moved public and src folders into client folder as needed

--- tortilla/server.js
+++ tortilla/server.js
@@ -1,17 +1,26 @@
 const express = require('express');
 
 // Sets up the Express App
+const mongoose = require('mongoose');
+const routes = require('./routes');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 // Requiring our models for syncing
 const db = require('./models');
 
-// Sets up the Express app to handle data parsing
+// Sets up the Express app to handle data parsing-Define middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//Serve up static assets (usually on heroku)
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+//Add routes, both API and view
+app.use(routes);
 
 // Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+  app.listen(PORT, () => 
+  console.log(`Listening on PORT ${PORT}`));
+
