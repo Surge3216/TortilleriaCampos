5c4222f7e53d62ec9caf1889c80248479c7e05dd
Aubrey Mark <aubsmark@gmail.com>
2021-05-11 20:32:56 -0600

    added some needed folders and a little helper code to Deliveriers page:

--- tortilla/src/App.js
+++ tortilla/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./pages/Login"
 import Story from "./pages/Story"
 import Contact from "./pages/Contact";
+import DriversOrders from "./pages/DriversOrders";
 
 
 class App extends Component{
@@ -18,6 +19,7 @@ class App extends Component{
         <Route exact path="/story" component={Story} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/contact" component={Contact} />
+        <Route exact path="/driverorders" component={DriversOrders}/>
         <Footer />
       </div>
     </Router>
