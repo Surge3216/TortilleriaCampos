c30f609f7e90c3b4a997a08782f0ede0c89c9090
Sergio <sergiocmps7@gmail.com>
2021-05-12 21:31:36 -0600

    fix

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./pages/Login"
+import Story from "./pages/Story"
+import Contact from "./pages/Contact";
+import Carousel from "./components/Carousel/index"
+import row from "./components/Row/index"
+import Row from "./components/Row/index";
+import DriversOrders from "./pages/DriversOrders"
+
+class App extends Component{
+
+  render() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Row>
+          <div class="col l12 offset-l1">
+        <Carousel />
+        </div>
+        </Row>
+        <Route exact path="/" component={Story}/>
+        <Route exact path="/story" component={Story} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/driverorders" component={DriversOrders}/>
+        <Footer />
+      </div>
+    </Router>
+
+  );
+}
+}
+
+export default App;
