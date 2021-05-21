ba231517ab238b5b6e18d03497f027ef4643b303
Aubrey Mark <aubsmark@gmail.com>
2021-05-20 23:16:53 -0600

    added controller pages still need to work on them

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,16 @@
 const path = require("path");
 const router = require("express").Router();
 const orderRoutes = require("./orders");
+const customerRoutes = require("./customer");
+const employeeRoutes = require("./employee");
+const newclientRoutes = require("./newclient");
 
 // API Routes
 router.use("/orders", orderRoutes);
+router.use("/customer", customerRoutes);
+router.use("/employee", employeeRoutes);
+router.use("/newclient", newclientRoutes);
+
 
 
 // If no API routes are hit, send the React app
