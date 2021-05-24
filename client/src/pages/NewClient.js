a44c27201e2dfa27092267d03b23abb3ac38cb5a
Sergio <sergiocmps7@gmail.com>
2021-05-24 13:13:00 -0600

    changes to new client page

--- client/src/pages/NewClient.js
+++ client/src/pages/NewClient.js
@@ -5,45 +5,43 @@ function NewClient() {
     return (
         <div>
 
-            <Row>
-            <div className="col l4 offset-l2 m6 offset-m1">
-                    <h5>Enter in your business information below!</h5>
-                    
-                    <div class="card-panel teal white">
-                    <Row>
-                    <form className="col s12">
+            <Row/>                  
+                    <div class="card-panel col l9 offset-l1 teal white">
+                    <form className="">
                         <div className="row">
-                            <div className="input-field col l10 m8 s8 ">
-                                <input placeholder="Name" id="name" type="text" class="validate"></input>
+                        <div className=" input-field col l4 offset-l2">
+                                <input placeholder="Business Name" id="ClientName" type="text" class="validate "></input>
                             </div>
-                            <div className=" input-field col l10 m8 s8">
-                                <input placeholder="Email" id="Email" type="text" class="validate "></input>
+                            <div className=" input-field col l4 ">
+                                <input placeholder="Email" id="clientEmailAddress" type="text" class="validate "></input>
                             </div>
-                            <div className=" input-field col l10 m8 s8">
-                                <input placeholder="Business Name" id="Subject" type="text" class="validate "></input>
+                            <div className=" input-field col l4 offset-l2">
+                                <input placeholder="Phone number" id="clientNumber" type="text" class="validate "/>
                             </div>
-                            <div className=" input-field col l12 m8 s8">
-                                <textarea id="Your Order" type="text" class="validate "/>
+                            <div className=" input-field col l4 ">
+                                <input placeholder="Busness type" id="clientType" type="text" class="validate "/>
                             </div>
+                            <div className=" input-field col l4 offset-l2">
+                                <input placeholder="address" id="clientStreetAddress" type="text" class="validate "/>
+                            </div>
+                            <div className=" input-field col l2 ">
+                                <input placeholder="City" id="clientCity" type="text" class="validate "/>
+                            </div>
+                            <div className=" input-field col l1 ">
+                                <input placeholder="State" id="clientState" type="text" class="validate "/>
+                            </div>
+                            <div className=" input-field col l1 ">
+                                <input placeholder="Zip" id="clientZip" type="text" class="validate "/>
+                            </div>
+
                         </div>
-                        {/* <div className="row">
-                            <div className=" input-field col l8 m8 s8">
-                                <input placeholder="password" id="user_password" type="text" class="validate "></input>
-                            </div>
-                        </div> */}
                         <div className="row">
-                            <div class="col">
-                            <a class="waves-effect waves-light btn">Send</a>
+                            <div class="col l1 offset-l9">
+                            <a class="waves-effect waves-light btn">Add</a>
                             </div>
                         </div>
                     </form>
-                    </Row>
-                </div>
-                </div>
-                <div className="col l5">
-                  
-                </div>
-            </Row>            
+                </div>         
         </div>
     );
 };
